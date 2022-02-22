586ebb52661021aa1c0ed96faa02d2698dbac7e1
Maxim Ziatdinov <ziatdinovmax@gmail.com>
2022-02-21 20:49:28 -0500

    Make sure that meanfn priors are passed to ExactGP

--- gpax/vgp.py
+++ gpax/vgp.py
@@ -31,7 +31,7 @@ class vExactGP(ExactGP):
                  mean_fn_prior: Optional[Callable[[], Dict[str, jnp.ndarray]]] = None,
                  noise_prior: Optional[Callable[[], Dict[str, jnp.ndarray]]] = None
                  ) -> None:
-        args = (input_dim, kernel, mean_fn, kernel_prior, noise_prior)
+        args = (input_dim, kernel, mean_fn,  kernel_prior, mean_fn_prior, noise_prior)
         super(vExactGP, self).__init__(*args)
 
     def model(self, X: jnp.ndarray, y: jnp.ndarray) -> None:
